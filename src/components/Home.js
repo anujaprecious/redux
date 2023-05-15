5388e161acac5dc3bd1629293030d70d96cc4f40
anujaprecious <anuja.patel@preciousinfosystem.com>
2023-05-15 12:59:37 +0530

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+function Home(props) {
+    return (
+        <div>
+
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="iphone.png" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button
+                    onClick={
+                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+
+                        <button className='remove-btn'
+                    onClick={
+                        ()=>{props.removeToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Remove To Cart</button>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default Home
